1d5332e4e8495c651cbb4cd74358bff263ee8977
SrinivasRajaraman <rsri1011@gmail.com>
2017-03-16 12:07:54 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton.Singleton.InstanceVar.Foo();
+            Singleton.Singleton.InstanceVar.Foo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+         
+        private static Singleton instanceVar = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {   
+        }
+
+        public static Singleton InstanceVar
+        {
+            get
+            {
+                //threadsafe implementation of Singleton Pattern.
+                lock (padlock)
+                {
+                    //Create an instance only if the instance variable is null.
+                    if (instanceVar == null)
+                        Console.WriteLine("Creating a new instance");
+                        instanceVar = new Singleton();
+                    
+                    return instanceVar;
+                }
+            }
+        }
+
+        //Test method
+        internal void Foo()
+        {
+            Console.WriteLine("Inside foo method");
+        }
+
+
+    }
+}
